74a8a23c857440bfaca52b2ccf71095694a9da69
Wylie Kuhn <86081235+WylieKuhn@users.noreply.github.com>
2023-04-20 19:38:11 -0400

    Added error handing demonstration file and code

--- complete_transactions.py
+++ complete_transactions.py
@@ -34,76 +34,106 @@ def complete_transactions():
             sender_id = sent_transaction[1]
             expected_amount = float(receieved_transaction[3])
             paid_amount = float(sent_transaction[2])
+            #get time of transaction
+            timestamp = str(sent_transaction[4])
             
             #verify transactions match
-            if receiver_key == sender_key and sender_id == expected_sender_id and expected_amount == paid_amount:
-                #get time of transaction
-                timestamp = str(sent_transaction[4])
-                
-                
-                #get sender's account info
+            if receiver_key == sender_key:
                 accounts_cursor.execute(f"SELECT * FROM accounts WHERE user_id = {sender_id}")
                 sender_account_info = accounts_cursor.fetchone()
-                #get receiver's account info
                 accounts_cursor.execute(f"SELECT * FROM accounts WHERE user_id = {receiever_id}")
                 receiver_account_info = accounts_cursor.fetchone()
+                if sender_id == expected_sender_id and expected_amount == paid_amount:
+                    
+                    
+                    #get sender's account info
+                    
+                    #get receiver's account info
+                    accounts_cursor.execute(f"SELECT * FROM accounts WHERE user_id = {receiever_id}")
+                    receiver_account_info = accounts_cursor.fetchone()
+                    
+                    sender_balance = float(sender_account_info[2])
+                    
+
+                    #make sure sender's account balance is enough to cover the amount being sent
+                    if sender_balance > paid_amount:
+                        receiver_balance = float(receiver_account_info[2])
+
+                        # update account balances of the users
+                        new_sender_balance = sender_balance - paid_amount
+                        new_reciever_balance = receiver_balance + paid_amount
+
+                        accounts_cursor.execute(
+                            f"UPDATE accounts SET account_balance = {new_sender_balance} WHERE user_id = {int(sender_id)}")
+                        accounts_connection.commit()
+                        accounts_cursor.execute(
+                            f"UPDATE accounts SET account_balance = {new_reciever_balance} WHERE user_id = {int(receiever_id)}")
+                        accounts_connection.commit()
+
+                        #insert final permanent trasaction record into the finished transactions databse
+                        finished.execute(F"""INSERT INTO finished_transactions (account_number_sender, account_number_receiver,
+                                            transaction_timestamp, transfer_amount, beginning_balance_sender, bank_sender, os_sender, os_receiver,
+                                            unanimous_agreement, transaction_error) VALUES ({int(sender_account_info[0])}, {int(receiver_account_info[0])}, 
+                                            '{timestamp}', {float(paid_amount)}, {receiver_balance}, '{str(sender_account_info[4])}',
+                                            '{sent_transaction[3]}', '{receieved_transaction[4]}', 1, 0)""")
+                        finished_transactions.commit()
+                        
+                        #delete transaction verification record from temporary transaction database
+                        sender_database_cursor.execute(
+                            f"DELETE FROM sent_transactions WHERE key = '{str(sender_key)}'")
+                        sender_dastabase_connection.commit()
+                        
+                        receiver_database_cursor.execute(f"DELETE FROM received_transactions WHERE key = '{str(receiver_key)}'")
+                        receiver_dastabase_connection.commit()
+                        print(f"COMPLETED TRANSACTION {receiver_key}")
+                    
+                    #deny and log error if balance is not enough to cover paid amount
+                    elif sender_balance < paid_amount:
+                        print(f"ERROR CODE 1OO, TRANSACTION FAILURE DUE TO ATTEMPTED OVERDRAW: {sender_key}")
+                        receiver_balance = float(receiver_account_info[2])
+                        error = "ATTEMPTED OVERDRAW: Paid amount exceeds balance"
+
+                        #insert final permanent trasaction record into the finished transactions databse
+                        error_database_cursor.execute(F"""INSERT INTO errors (account_number_sender, account_number_receiver,
+                                            transaction_timestamp, transfer_amount, beginning_balance_sender, bank_sender, os_sender, os_receiver,
+                                            unanimous_agreement, error_explanation, error_code) VALUES ({int(sender_account_info[0])}, 
+                                            {int(receiver_account_info[0])}, '{timestamp}', {float(sent_transaction[2])}, {sender_account_info[2]}, 
+                                            '{str(sender_account_info[4])}', '{sent_transaction[3]}', '{receieved_transaction[4]}', 1, '{str(error)}', 100)""")
+                        error_database_connection.commit()
+                        
+                        #delete transaction verification record from temporary transaction database
+                        sender_database_cursor.execute(
+                            f"DELETE FROM sent_transactions WHERE key = '{sender_key}'")
+                        sender_dastabase_connection.commit()
+                        
+                        receiver_database_cursor.execute(f"DELETE FROM received_transactions WHERE key = '{receiver_key}'")
+                        receiver_dastabase_connection.commit()
+            if sender_id != expected_sender_id:
+                error = "ERROR CODE 200, SENDER ID MISMATCH: "
+                error_database_cursor.execute(F"""INSERT INTO errors (account_number_sender, account_number_receiver,
+                                transaction_timestamp, transfer_amount, beginning_balance_sender, bank_sender, os_sender, os_receiver,
+                                unanimous_agreement, error_explanation, error_code, transaction_id) VALUES ({int(sender_account_info[0])}, 
+                                {int(receiver_account_info[0])}, '{timestamp}', {float(sent_transaction[2])}, {sender_account_info[2]}, 
+                                '{str(sender_account_info[4])}', '{sent_transaction[3]}', '{receieved_transaction[4]}', 1, '{str(error)}', 200, '{sender_key}')""")
+            error_database_connection.commit()
+            if expected_amount != paid_amount:
+                error = "ERROR CODE 300, TRANSACTION AMOUNT MISMATCH: "
                 
-                sender_balance = float(sender_account_info[2])
+            error_database_cursor.execute(F"""INSERT INTO errors (account_number_sender, account_number_receiver,
+                                transaction_timestamp, transfer_amount, beginning_balance_sender, bank_sender, os_sender, os_receiver,
+                                unanimous_agreement, error_explanation, error_code, transaction_id) VALUES ({int(sender_account_info[0])}, 
+                                {int(receiver_account_info[0])}, '{timestamp}', {float(sent_transaction[2])}, {sender_account_info[2]}, 
+                                '{str(sender_account_info[4])}', '{sent_transaction[3]}', '{receieved_transaction[4]}', 1, '{str(error)}', 300, '{sender_key}')""")
+            error_database_connection.commit()
+            
+            sender_database_cursor.execute(
+                f"DELETE FROM sent_transactions WHERE key = '{sender_key}'")
+            sender_dastabase_connection.commit()
+            
+            receiver_database_cursor.execute(f"DELETE FROM received_transactions WHERE key = '{receiver_key}'")
+            receiver_dastabase_connection.commit()
                 
-
-                #make sure sender's account balance is enough to cover the amount being sent
-                if sender_balance > paid_amount:
-                    receiver_balance = float(receiver_account_info[2])
-
-                    # update account balances of the users
-                    new_sender_balance = sender_balance - paid_amount
-                    new_reciever_balance = receiver_balance + paid_amount
-
-                    accounts_cursor.execute(
-                        f"UPDATE accounts SET account_balance = {new_sender_balance} WHERE user_id = {int(sender_id)}")
-                    accounts_connection.commit()
-                    accounts_cursor.execute(
-                        f"UPDATE accounts SET account_balance = {new_reciever_balance} WHERE user_id = {int(receiever_id)}")
-                    accounts_connection.commit()
-
-                    #insert final permanent trasaction record into the finished transactions databse
-                    finished.execute(F"""INSERT INTO finished_transactions (account_number_sender, account_number_receiver,
-                                        transaction_timestamp, transfer_amount, beginning_balance_sender, bank_sender, os_sender, os_receiver,
-                                        unanimous_agreement, transaction_error) VALUES ({int(sender_account_info[0])}, {int(receiver_account_info[0])}, 
-                                        '{timestamp}', {float(paid_amount)}, {receiver_balance}, '{str(sender_account_info[4])}',
-                                        '{sent_transaction[3]}', '{receieved_transaction[4]}', 1, 0)""")
-                    finished_transactions.commit()
-                    
-                    #delete transaction verification record from temporary transaction database
-                    sender_database_cursor.execute(
-                        f"DELETE FROM sent_transactions WHERE key = '{str(sender_key)}'")
-                    sender_dastabase_connection.commit()
-                    
-                    receiver_database_cursor.execute(f"DELETE FROM received_transactions WHERE key = '{str(receiver_key)}'")
-                    receiver_dastabase_connection.commit()
-                    print(f"COMPLETED TRANSACTION {receiver_key}")
-                
-                #deny and log error if balance is not enough to cover paid amount
-                elif sender_balance < paid_amount:
-                    print(f"TRANSACTION FAILURE DUE TO ATTEMPTED OVERDRAW: {sender_key}")
-                    receiver_balance = float(receiver_account_info[2])
-                    error = "ATTEMPTED OVERDRAW: Paid amount exceeds balance"
-
-                    #insert final permanent trasaction record into the finished transactions databse
-                    error_database_cursor.execute(F"""INSERT INTO errors (account_number_sender, account_number_receiver,
-                                        transaction_timestamp, transfer_amount, beginning_balance_sender, bank_sender, os_sender, os_receiver,
-                                        unanimous_agreement, error_explanation) VALUES ({int(sender_account_info[0])}, 
-                                        {int(receiver_account_info[0])}, '{timestamp}', {float(sent_transaction[2])}, {sender_account_info[2]}, 
-                                        '{str(sender_account_info[4])}', '{sent_transaction[3]}', '{receieved_transaction[4]}', 1, '{str(error)}')""")
-                    error_database_connection.commit()
-                    
-                    #delete transaction verification record from temporary transaction database
-                    sender_database_cursor.execute(
-                        f"DELETE FROM sent_transactions WHERE key = '{sender_key}'")
-                    sender_dastabase_connection.commit()
-                    
-                    receiver_database_cursor.execute(f"DELETE FROM received_transactions WHERE key = '{receiver_key}'")
-                    receiver_dastabase_connection.commit()
+                            
                     
 while(True):
     complete_transactions()
