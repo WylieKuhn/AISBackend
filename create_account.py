79b97e917a1ff0727867740d783350ac4c5d6da0
Wylie Kuhn <86081235+WylieKuhn@users.noreply.github.com>
2023-04-22 00:22:26 -0400

    Added https security

--- create_account.py
+++ create_account.py
@@ -1,5 +1,13 @@
 import requests
 import random
+import ssl
+import warnings
+from requests.packages.urllib3.exceptions import SubjectAltNameWarning
+
+warnings.filterwarnings("ignore", category=SubjectAltNameWarning)
+
+context = ssl.create_default_context()
+context.load_verify_locations('cert.pem')
 
 
 
@@ -12,6 +20,6 @@ def create_account():
     bank_sender = str(input("Bank sender: "))
     
     new_account = {"user_id": user_id, "account_number": account_number, "balance": balance, "account_type": account_type, "bank_sender": bank_sender}
-    requests.post("http://127.0.0.1:8000/api/v1/create/", json=new_account)
+    requests.post("https://127.0.0.1:8000/api/v1/create/", json=new_account, verify="cert.pem")
     
 create_account()
